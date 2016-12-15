285563739f158e6db14030d978d44500ff5420fc
Slava <development1810@gmail.com>
2016-12-15 21:15:00 +0200

    PRICE PRODUCT NAME Trim ":" from property names Type of Currency

--- aliExpress/aliExpress.go
+++ aliExpress/aliExpress.go
@@ -1,8 +1,10 @@
 package aliExpress
 
 import (
+	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/dennwc/gcrawl"
+	"log"
 	"strings"
 )
 
@@ -94,10 +96,10 @@ func GetList(v string) ([]string, string, error) {
 	var np string
 	doc.Find("a.page-next").Each(func(i int, s *goquery.Selection) {
 		/*
-				Also to doc.Find(".ui-pagination-navi.util-left>a:last-child") this selector works great too
-					:last-child - псевдокласс
-		 			типа последний элемент которому подходит селектор
-		 			в родительськом элементе
+					Also to doc.Find(".ui-pagination-navi.util-left>a:last-child") this selector works great too
+						:last-child - псевдокласс
+			 			типа последний элемент которому подходит селектор
+			 			в родительськом элементе
 		*/
 		link, ok := s.Attr("href")
 		if !ok {
@@ -121,12 +123,55 @@ func GetAliSpecification(url string) (*gcrawl.Object, error) {
 	doc.Find(".ui-box-body .product-property-list.util-clearfix li").Each(func(i int, s *goquery.Selection) {
 		left := strings.TrimSpace(s.Find(".propery-title").Text())
 		right := strings.TrimSpace(s.Find(".propery-des").Text())
+		left = strings.Trim(left, ":")
+		//	log.Printf("LEFT = '%v'\n", left)
+		//	log.Printf("RIGHT = '%v'\n", right)
 		var data gcrawl.Value
 		data = gcrawl.String(right)
 		slice = append(slice, gcrawl.Property{
 			ID:    "",
 			Name:  left,
 			Value: data,
+		})
+	})
+	doc.Find(".detail-wrap").Each(func(i int, s *goquery.Selection) {
+		title := s.Find(".product-name").Text()
+		log.Printf("title = '%v'\n", title)
+		//		price := s.Find(".product-price").Text()
+		var data3 gcrawl.Value
+		data3 = gcrawl.String(title)
+		slice = append(slice, gcrawl.Property{
+			ID:    "",
+			Name:  "title",
+			Value: data3,
+		})
+	})
+	doc.Find(".product-price").Each(func(i int, s *goquery.Selection) {
+		/*>input#sku-price-store*/
+		//s.Find(“.product-price [itemprop='priceCurrency']”).getAttribute(“content”)
+		price := s.Find("#j-sku-price").Text()
+		price = strings.Replace(price, ",", ".", -1)
+		price = strings.Replace(price, " ", "", -1)
+		log.Printf("price = '%v'\n", price)
+		log.Printf("price2 = '%x' '%x'", price, " ")
+		var data2 gcrawl.Value
+		data2 = gcrawl.String(price)
+		slice = append(slice, gcrawl.Property{
+			ID:    "",
+			Name:  "price",
+			Value: data2,
+		})
+	})
+	//doc.Find(".product-price").Each(func(i int, s *goquery.Selection){
+	doc.Find(".p-symbol").Each(func(i int, s *goquery.Selection) {
+		a, _ := s.Attr("content")
+		fmt.Printf("a = '%v'\n", a)
+		var currency gcrawl.Value
+		currency = gcrawl.String(a)
+		slice = append(slice, gcrawl.Property{
+			ID:    "",
+			Name:  "currency",
+			Value: currency,
 		})
 	})
 	return &gcrawl.Object{
